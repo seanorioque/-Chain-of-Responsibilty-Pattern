cf0133f08bee859b83d9520f937e1406ef3defc1
Sean Orioque <seanorioque23@gmail.com>
2026-02-24 00:54:17 +0000

    Created Responsibility Design Pattern

---
+++ ATMDispenseChain.java
@@ -0,0 +1,31 @@
+public class ATMDispenseChain implements DispenseChain {
+    private DispenseChain nextChain;
+    public ATMDispenseChain() {
+        // Initialize the chain
+        this.nextChain = new Peso1000Dispenser();
+        DispenseChain c2 = new Peso500Dispenser();
+        DispenseChain c3 = new Peso200Dispenser();
+        DispenseChain c4 = new Peso100Dispenser();
+        DispenseChain c5 = new Peso50Dispenser();
+        DispenseChain c6 = new Peso20Dispenser();
+
+        // Set the chain of responsibility
+        nextChain.setNextChain(c2);
+        c2.setNextChain(c3);
+        c3.setNextChain(c4);
+        c4.setNextChain(c5);
+        c5.setNextChain(c6);
+        
+    }
+
+
+    public void dispense(Currency currency) {
+        nextChain.dispense(currency);
+    }
+
+
+    @Override
+    public void setNextChain(DispenseChain nextChain) {
+        this.nextChain = nextChain;
+    }
+}
